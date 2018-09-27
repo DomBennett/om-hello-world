e91406d6d7e5b5676d5f5de76017e774271671bf
Dominic Bennett <dominic.john.bennett@gmail.com>
2018-09-27 20:57:44 +0200

    updates

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,5 @@
+pckgnm <- environmentName(env = environment())
+
 #' @name hello_world
 #' @title Say hello!
 #' @description Run \code{echo "Hello World!"} in Ubuntu 16.04.
@@ -5,15 +7,13 @@
 #' @return Logical
 #' @export
 hello_world <- function(...) {
-  base_id <- 'hello-world'
-  img_id <- paste0('dombennett/', base_id)
-  # generate unique container ID by counting processes
-  nps <- outsider:::docker_ps_count()
-  cntnr_id <- paste0(base_id, '_', nps)
+  ids <- outsider:::ids_get(pckgnm = pckgnm)
   # launch container
-  outsider:::docker_start(cntnr_id = cntnr_id, img_id = img_id)
-  on.exit(outsider:::docker_stop(cntnr_id = cntnr_id))
+  outsider:::docker_start(cntnr_id = ids[['cntnr_id']],
+                          img_id = ids[['img_id']])
+  on.exit(outsider:::docker_stop(cntnr_id = ids[['cntnr_id']]))
   # run command
-  invisible(outsider:::docker_exec(cntnr_id = cntnr_id, 'echo', 'Hello World!',
-                                   ...))
+  invisible(outsider:::docker_exec(cntnr_id = ids[['cntnr_id']], 'echo',
+                                   'Hello World!', ...))
 }
+
