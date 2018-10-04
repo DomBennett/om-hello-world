5a3ed4ea1105cab5ad5bd797a966acf09473cac1
Dom Bennett <dominic.john.bennett@gmail.com>
2018-10-04 14:11:14 +0200

    example added

--- R/functions.R
+++ R/functions.R
@@ -4,6 +4,7 @@ pkgnm <- environmentName(env = environment())
 #' @title Say hello!
 #' @description Run \code{echo "Hello World!"} in Ubuntu 18.04.
 #' @return Logical
+#' @example examples/hello_world.R
 #' @export
 hello_world <- function() {
   outsider::.run(pkgnm = pkgnm, files_to_send = NULL, 'echo', 'Hello World!')
---
+++ examples/hello_world.R
@@ -0,0 +1,4 @@
+library(outsider)
+hello_world <- module_import('hello_world',
+                             repo = 'dombennett/om..hello.world..1.0')
+hello_world()
