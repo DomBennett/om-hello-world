9dc2ee0d2a12f13129281c7c1212f90644b61aae
Dom Bennett <dominic.john.bennett@gmail.com>
2018-11-22 14:15:53 +0100

    example fix

--- examples/hello_world.R
+++ examples/hello_world.R
@@ -1,4 +1,4 @@
 library(outsider)
 hello_world <- module_import('hello_world',
-                             repo = 'dombennett/om..hello.world..1.0')
+                             repo = 'dombennett/om..hello.world')
 hello_world()
