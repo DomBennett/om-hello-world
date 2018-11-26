066d10873ec60c9acdbfae9d99f2b133b5e82ba3
Dom Bennett <dominic.john.bennett@gmail.com>
2018-11-26 15:48:04 +0100

    updates

--- R/functions.R
+++ R/functions.R
@@ -1,5 +1,3 @@
-pkgnm <- environmentName(env = environment())
-
 #' @name hello_world
 #' @title Say hello!
 #' @description Run \code{echo "Hello World!"} in Ubuntu 18.04.
@@ -7,5 +5,7 @@ pkgnm <- environmentName(env = environment())
 #' @example examples/hello_world.R
 #' @export
 hello_world <- function() {
-  outsider::.run(pkgnm = pkgnm, cmd = 'cat', args = '../hello.txt')
+  otsdr <- outsider::.outsider_init(repo = 'dombennett/om..hello.world',
+                                    cmd = 'cat', arglist = '../hello.txt')
+  outsider::.run(otsdr)
 }
